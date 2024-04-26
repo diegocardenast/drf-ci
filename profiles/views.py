d36ee0055f53d364e25817a5e03b4250d48a660e
Diego Cardenas <diego.cardenast@gmail.com>
2024-04-26 22:50:57 +0000

    added profile filters with django-filters and updated requirements

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_ci.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
+
 
 class ProfileList(generics.ListAPIView):
     """
@@ -12,16 +14,22 @@ class ProfileList(generics.ListAPIView):
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following',distinct=True)
+        following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
     ]
     ordering_fields = [
         'posts_count',
         'followers_count',
-        'following_count'
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
     ]
 
 
@@ -33,6 +41,6 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following',distinct=True)
+        following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
